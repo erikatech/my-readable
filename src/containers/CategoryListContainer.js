460b26784cbab9b3f8bb993489f18c38082c0233
Erika Silva <erika.silva@itriad.org.br>
2017-10-31 18:32:50 -0400

    refactor: remove unused files and create new ones

---
+++ src/containers/CategoryListContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import CategoryList from "../presentational/CategoryList";
+import {fetchCategories} from "../actions/categoryActions";
+import {withRouter} from "react-router";
+import {connect} from "react-redux";
+import {push} from "react-router-redux";
+
+class CategoryListContainer extends Component {
+
+	componentDidMount(){
+		this.props.requestCategories();
+	}
+
+	render(){
+		const selectedCategory = this.props.location.pathname.replace("/", "");
+		return (
+		  <CategoryList
+		    selectedCategory={selectedCategory}
+		    selectCategory={ this.props.selectCategory }
+		    categories={ this.props.categories }
+		  />
+		)
+	}
+}
+
+function mapStateToProps(state){
+	return {
+		categories: state.category.categories
+	};
+}
+
+function mapDispatchToProps(dispatch){
+	return {
+		requestCategories: () => dispatch(fetchCategories()),
+		selectCategory: (selectedCategory) => dispatch(push(`/${selectedCategory}`))
+	}
+}
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CategoryListContainer));
