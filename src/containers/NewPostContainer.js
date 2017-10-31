f35b902eaf43929473e58a5e9e4c4327b65e002c
Erika Silva <erika.silva@itriad.org.br>
2017-10-31 18:42:01 -0400

    feat: create the page to add a new post

---
+++ src/containers/NewPostContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class NewPostContainer extends Component {
+	render(){
+
+		return (
+		  <h1>Olá novo post</h1>
+		)
+	}
+}
+
+export default NewPostContainer;
