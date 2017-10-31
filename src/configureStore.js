29fa6ce445fff7aa2accfb3a6f2fdf46bdd1873e
Erika Silva <erika.silva@itriad.org.br>
2017-10-31 18:36:47 -0400

    feat: add the file that deals with the app store

---
+++ src/configureStore.js
@@ -0,0 +1,35 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+
+import createHistory from 'history/createBrowserHistory';
+
+import thunkMiddleware from 'redux-thunk';
+
+import { routerMiddleware} from 'react-router-redux'
+
+import reducer from './reducers';
+
+export const history = createHistory();
+
+const initialState = {};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = [];
+
+const middleware = [
+	thunkMiddleware,
+	routerMiddleware(history)
+];
+
+const composedEnhancers = composeEnhancers(
+  applyMiddleware(...middleware),
+  ...enhancers
+);
+
+const store = createStore(
+  reducer,
+  initialState,
+  composedEnhancers
+);
+
+export default store;
