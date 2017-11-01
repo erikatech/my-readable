b9fd584e6d9c954dab9b1a83776ea4b07fd2ad38
Erika Silva <erika.silva@itriad.org.br>
2017-11-01 14:22:57 -0400

    feat: create the submit post feature

--- src/containers/NewPostContainer.js
+++ src/containers/NewPostContainer.js
@@ -1,12 +1,106 @@
 import React, {Component} from 'react';
+import CategoryList from "../presentational/CategoryList";
+import {withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import * as ReadableAPI from '../utils/ReadableAPI';
+import {generateUUID} from "../utils/UUIDGenerator";
+import {getSinglePost} from "../actions/postActions";
 
 class NewPostContainer extends Component {
-	render(){
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			post: {title: "", author: "", body: "", category: ""}
+		}
+	}
+
+	componentDidMount(){
+		const postId = this.props.match.params.postId;
+		if(postId){
+			this.props.getSinglePost(postId);
+		}
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.currentPost){
+			this.setState({post: nextProps.currentPost});
+		}
+
+	}
+
+	handleInputChange = (key, value) => {
+		this.setState(state => {
+			state.post[key] = value;
+			return state;
+		})
+	};
+
+	sendPost = () => {
+		const { post } = this.state;
+		post.timestamp = new Date().getTime();
+
+		if(post.id){
+			ReadableAPI.updatePost(post);
+		} else {
+			post.id = post.id || generateUUID();
+			ReadableAPI.sendPost(post)
+			  .then(() => {
+				  this.setState({
+					  post: {
+						  id: "",
+						  author: "",
+						  body: "",
+						  title: "",
+						  category: ""
+					  }
+				  });
+			  })
+		}
+	};
+
+	render() {
+		const {post} = this.state;
 		return (
-		  <h1>Olá novo post</h1>
+		  <div>
+			  <h1>{post.id ? 'Edit' : 'New'} Post</h1>
+			  <label>Title</label>
+			  <input type="text" value={post.title}
+			         onChange={(e) => this.handleInputChange('title', e.target.value)}/><br/>
+
+			  <label>Author</label>
+			  <input type="text" value={post.author}
+			         onChange={(e) => this.handleInputChange('author', e.target.value)}/><br/>
+
+
+			  <label>Content</label>
+			  <input type="text" value={post.body}
+			         onChange={(e) => this.handleInputChange('body', e.target.value)}/><br/>
+
+			  <CategoryList
+			    selectedCategory={post.category}
+				selectCategory={(category) => this.handleInputChange('category', category)}
+				categories={this.props.categories}
+			  /><br/>
+
+			  <input type="button" onClick={this.sendPost} value="Submit"/>
+		  </div>
 		)
 	}
 }
 
-export default NewPostContainer;
+function mapStateToProps(state) {
+	return {
+		categories: state.category.categories,
+		currentPost: state.post.currentPost
+	}
+}
+
+function mapDispatchToProps(dispatch){
+	return {
+		getSinglePost: (postId) => dispatch(getSinglePost(postId))
+	}
+}
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NewPostContainer));
