cd2fb57fca6040f00fd3bb040140342192fae6f7
Erika Silva <erika.silva@itriad.org.br>
2017-11-06 15:05:48 -0400

    refactor: replace the component with its inner content [unecessary component]

--- src/presentational/Post.js
+++ src/presentational/Post.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import PostActionButtonsContainer from "../containers/PostActionButtonsContainer";
 import Vote from "./Vote";
+import {Link} from "react-router-dom";
+import {convertToStringDate} from "../utils/AppUtils";
 
-const Post = ({post, onVote}) => {
+const Post = ({post, onVote, showBody, commentsCount, onRemove}) => {
+	const urlToComment = `/${post.category}/comment/${post.id}`;
+	const urlToEdit = `/new/${post.id}`;
+
 	return (
-
-	  <div>
+	  <div className="post-container">
 		  <Vote voteScore={post.voteScore} onVote={(option) => onVote(option, post)}/>
 
-		  <h3>{post.title}</h3>
-		  <p>{post.content}</p>
-		  <span>/{post.category}</span>
+		  <div className="post-content">
+			  <h3>{post.title}</h3>
 
-		  <PostActionButtonsContainer post={post} />
-		  <hr/>
+			  {showBody && (
+			    <p>{post.body}</p>
+			  )}
+
+			  <span>submitted by <strong>{post.author}</strong> to <strong>/{post.category}</strong> on {convertToStringDate(post.timestamp)}
+			  </span>
+
+			  <div>
+				  <Link to={urlToComment}>{commentsCount !== undefined ? commentsCount : post.commentCount} comments </Link>
+				  <Link to={urlToEdit}>Edit</Link>
+
+				  {onRemove && (
+				    <button onClick={() => onRemove(post)}>Remove</button>
+				  )}
+			  </div>
+		  </div>
 	  </div>
 	)
 };
