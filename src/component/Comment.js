82a0bed5709092b6a89dd655f494cf0b955b69a2
Erika Silva <erika.silva@itriad.org.br>
2017-11-09 10:32:52 -0400

    refactor: sort comments by vote score and add comments

--- src/component/Comment.js
+++ src/component/Comment.js
@@ -6,10 +6,15 @@ import Post from "./Post";
 import SingleComment from "./SingleComment";
 import CommentForm from "./CommentForm";
 
+/**
+ * Component that represents the CommentsPage
+ */
 class Comment extends Component {
 
 	componentDidMount() {
+		// we get the postId from the params
 		const {postId} = this.props.match.params;
+		// fetch the post from the API, passing the postId from URL
 		this.props.getSinglePost(postId);
 	}
 
@@ -43,13 +48,22 @@ class Comment extends Component {
 }
 
 function mapStateToProps(state) {
+	// here we transform to a list so the comments can be sorted
+	const comments =  Object.keys(state.comment.comments).map(
+	  key => state.comment.comments[key]
+	);
 	return {
 		currentPost: state.post.currentPost,
-		comments: state.comment.comments
+		// sorts the comments by the voteScore
+		comments: comments.slice().sort((a, b) => b['voteScore'] - a['voteScore'])
 	}
 }
 
-
+/**
+ * mapping the required methods to props
+ * @param dispatch
+ * @returns {{getSinglePost: (function(*=): *), onVote: (function(*=, *): *)}}
+ */
 function mapDispatchToProps(dispatch) {
 	return {
 		getSinglePost: (postId) => dispatch(getSinglePost(postId)),
