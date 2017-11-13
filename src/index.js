d77f491575420d5fe847aed3eeb12a48f8a9d889
Erika Silva <erika.silva@itriad.org.br>
2017-11-13 10:25:15 -0400

    refactor: render App now, instead of rendering all the components directly

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Route, Switch} from 'react-router';
-import { ConnectedRouter } from 'react-router-redux'
+import { ConnectedRouter } from 'react-router-redux';
 import registerServiceWorker from './registerServiceWorker';
-import Header from "./component/Header";
-import store, { history } from "./configureStore";
+import store, { history } from "./store/index";
 import './resources/styles/css/index.css';
-import Home from "./component/Home";
-import Comment from "./component/Comment";
-import NewPost from "./component/NewPost";
+import App from "./component/App";
 
 ReactDOM.render(
   <Provider store={store}>
 	  <ConnectedRouter history={history}>
-		  <div>
-			  <Header />
-			  <Switch>
-				  <Route path="/new/:postId?" component={NewPost}/>
-				  <Route path="/:category/comment/:postId" component={Comment}/>
-				  <Route path="/:category?" component={Home}/>
-			  </Switch>
-		  </div>
+			<App />
 	  </ConnectedRouter>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
