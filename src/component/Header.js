781029100652de6f68fbf678407319388aeda58e
Erika Silva <erika.silva@itriad.org.br>
2017-11-13 10:24:30 -0400

    refactor: transform to functional stateless component

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
 import CategoryListContainer from "../containers/CategoryListContainer";
-import {withRouter} from 'react-router';
 import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-import {fetchCategories} from "../actions/categoryActions";
 
 /**
- * Used to render the header in all pages
+ * Functional component used to render to header of the application
+ * @returns {XML}
+ * @constructor
  */
-class Header extends Component {
-	componentDidMount() {
-		// makes a request to fetch the categories from the API
-		this.props.requestCategories();
-	}
-	render() {
-		return (
-		  <div className="header-container">
-			  <CategoryListContainer />
-			  <Link to="/">home</Link> |
-			  <Link
-				to="/new">
-				  new post
-			  </Link>
-		  </div>
-		);
-	}
-}
-function mapDispatchToProps(dispatch) {
-	return {
-		requestCategories: () => dispatch(fetchCategories())
-	}
-}
-export default withRouter(connect(null, mapDispatchToProps)(Header));
+const Header = () => {
+	return (
+	  <div className="header-container">
+		  <CategoryListContainer />
+		  <Link to="/">home</Link> |
+		  <Link
+			to="/new">
+			  new post
+		  </Link>
+	  </div>
+	);
+};
+export default Header;
